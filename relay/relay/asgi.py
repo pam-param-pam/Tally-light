25d6e674a87de666ad90bd88754a645c901888cc
JedrzejM <jedrzej.malachowskie@gmail.com>
2024-09-15 19:23:29 +0200

    typos

--- relay/relay/asgi.py
+++ relay/relay/asgi.py
@@ -13,7 +13,6 @@ django_asgi_app = get_asgi_application()
 from .consumers import TallyConsumer, AtemConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-# todo
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
 
