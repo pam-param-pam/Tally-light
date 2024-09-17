aef521d3e7f41fbd3a6d5350806e54a0e020ec40
JedrzejM <jedrzej.malachowskie@gmail.com>
2024-09-17 15:08:24 +0200

    fix DEBUG env

--- relay/relay/settings.py
+++ relay/relay/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
